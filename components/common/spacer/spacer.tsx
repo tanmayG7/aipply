a7dd69f900a3c45e7421058423890a2dc39cd179
Ganeshan Sudhakar <ganeshansudhakar01@gmail.com>
2025-05-07 19:48:08 +0530

    Remove codes

--- components/common/spacer/spacer.tsx
+++
@@ -1,3 +0,0 @@
-export default function Spacer() {
-  return <div className="mt-[47px] custom-md:mt-[95px] h-[1px] w-full" />;
-}
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'img.naukimg.com', "img.naukri.com"],
-  },
-};
-
-module.exports = nextConfig;
