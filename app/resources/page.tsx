33f815dccf3044bca085775efd1cddfe63ced79e
Tanmay Garg <tanmaygarg01@gmail.com>
2025-06-26 19:37:15 +0530

    Create page.tsx
    added page.tsx in resources

---
+++ app/resources/page.tsx
@@ -0,0 +1,14 @@
+tsx// app/resources/page.tsx
+import React from 'react';
+
+const ResourcesPage = () => {
+  return (
+    <div>
+      <h1>Resources</h1>
+      <p>Welcome to our resources section</p>
+      {/* Add links to your different resources here */}
+    </div>
+  );
+};
+
+export default ResourcesPage;
