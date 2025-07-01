3eaa5fdad9dfdf432b61ab3e7f4f4193e0f7983c
Tanmay Garg <tanmaygarg01@gmail.com>
2025-07-01 14:46:51 +0530

    Update page.tsx
    fixed image url and linkedin and instagram links

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -103,7 +103,7 @@ const AboutUsPage = () => {
             <div className="flex flex-col custom-md:flex-row justify-center gap-8 custom-md:gap-12 items-center">
               <div className="bg-[#111111] p-8 rounded-[20px] border border-[#333741] flex flex-col items-center">
                 <Image 
-                  src="/public/static/images/google-logo.png" 
+                  src="/static/images/google-logo.png" 
                   alt="Google for Startups" 
                   width={64}
                   height={64}
@@ -113,7 +113,7 @@ const AboutUsPage = () => {
               </div>
               <div className="bg-[#111111] p-8 rounded-[20px] border border-[#333741] flex flex-col items-center">
                 <Image 
-                  src="/public/static/images/bits-conquest-logo.png" 
+                  src="/static/images/bits-conquest-logo.png" 
                   alt="BITS Conquest" 
                   width={64}
                   height={64}
@@ -123,7 +123,7 @@ const AboutUsPage = () => {
               </div>
               <div className="bg-[#111111] p-8 rounded-[20px] border border-[#333741] flex flex-col items-center">
                 <Image 
-                  src="/public/static/images/wadhwani-foundation-logo.png" 
+                  src="/static/images/wadhwani-foundation-logo.png" 
                   alt="Wadhwani Foundation" 
                   width={64}
                   height={64}
@@ -149,7 +149,7 @@ const AboutUsPage = () => {
               {/* Tanmay Garg */}
               <div className="bg-[#111111] p-8 rounded-[30px] border border-[#333741]">
                 <Image 
-                  src="/public/static/images/tanmay-garg.jpg" 
+                  src="/static/images/tanmay-garg.jpg" 
                   alt="Tanmay Garg" 
                   width={96}
                   height={96}
@@ -165,7 +165,7 @@ const AboutUsPage = () => {
               {/* Disha Garg */}
               <div className="bg-[#111111] p-8 rounded-[30px] border border-[#333741]">
                 <Image 
-                  src="/public/static/images/disha-garg.jpg" 
+                  src="/static/images/disha-garg.jpg" 
                   alt="Disha Garg" 
                   width={96}
                   height={96}
@@ -247,16 +247,16 @@ const AboutUsPage = () => {
               </Link>
               <div className="bg-[#111111] p-6 rounded-[20px] border border-[#333741] flex-1">
                 <div className="flex items-center justify-center gap-4">
-                  <Link href="#" className="text-[#CECFD2] hover:text-[#52A9FF] transition-colors">
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-                    </svg>
-                  </Link>
-                  <Link href="#" className="text-[#CECFD2] hover:text-[#52A9FF] transition-colors">
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 5.079 3.158 9.417 7.618 11.174-.105-.949-.199-2.403.041-3.439.219-.937 1.406-5.957 1.406-5.957s-.359-.72-.359-1.781c0-1.663.967-2.911 2.168-2.911 1.024 0 1.518.769 1.518 1.688 0 1.029-.653 2.567-.992 3.992-.285 1.193.6 2.165 1.775 2.165 2.128 0 3.768-2.245 3.768-5.487 0-2.861-2.063-4.869-5.008-4.869-3.41 0-5.409 2.562-5.409 5.199 0 1.033.394 2.143.889 2.741.1.12.112.226.085.345-.09.375-.294 1.194-.334 1.362-.053.225-.172.271-.402.165-1.495-.69-2.433-2.878-2.433-4.646 0-3.776 2.748-7.252 7.92-7.252 4.158 0 7.392 2.967 7.392 6.923 0 4.135-2.607 7.462-6.233 7.462-1.214 0-2.357-.629-2.748-1.378 0 0-.599 2.282-.744 2.84-.282 1.054-1.059 2.37-1.575 3.179C9.447 23.67 10.693 24 12.017 24c6.625 0 11.99-5.367 11.99-11.987C24.007 5.367 18.642.001 12.017.001z"/>
-                    </svg>
-                  </Link>
+                <Link href="https://www.linkedin.com/company/aipply-io/" className="text-[#CECFD2] hover:text-[#52A9FF] transition-colors">
+  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+  </svg>
+</Link>
+<Link href="https://www.instagram.com/aipply.io/" className="text-[#CECFD2] hover:text-[#52A9FF] transition-colors">
+  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+  </svg>
+</Link>
                 </div>
                 <p className="font-manrope text-[16px] text-[#CECFD2] mt-2">Follow us on Social Media</p>
               </div>
