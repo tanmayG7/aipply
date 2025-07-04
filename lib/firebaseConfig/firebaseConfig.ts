085dc1ccfb8ab9b0db1e36e2d4e88c8b8e2ec9e5
Tanmay Garg <tanmaygarg01@gmail.com>
2025-07-05 02:27:51 +0530

    Update firebaseConfig.ts
    fixed syntax

--- lib/firebaseConfig/firebaseConfig.ts
+++ lib/firebaseConfig/firebaseConfig.ts
@@ -487,7 +487,6 @@ const getUpdatedJobsPaginated = async (
     experience?: [number, number][];
     jobType?: string[];
     platform?: string[]; 
-},
   },
   maxTotalJobs: number = 100 // NEW: Job limit parameter
 ) => {
