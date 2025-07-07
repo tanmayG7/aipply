66a2824cade3fc3b79c9a20034b94e052372a1d2
Tanmay Garg <tanmaygarg01@gmail.com>
2025-07-07 20:04:30 +0530

    Update yearlyComponent.tsx
    testing

--- components/sections/pricing/yearlyComponent/yearlyComponent.tsx
+++ components/sections/pricing/yearlyComponent/yearlyComponent.tsx
@@ -24,7 +24,7 @@ const YearlyComponent = ({ isVisible }: { isVisible: boolean }) => {
         if (form && form.children.length === 0) {
           const script = document.createElement('script');
           script.src = 'https://cdn.razorpay.com/static/widget/subscription-button.js';
-          script.setAttribute('data-subscription_button_id', 'pl_QqCnmIE8a89Pst');
+          script.setAttribute('data-subscription_button_id', 'pl_Qpqiazi0S9XVVD'); // Using monthly ID for testing
           script.setAttribute('data-button_theme', 'brand-color');
           script.async = true;
           form.appendChild(script);
