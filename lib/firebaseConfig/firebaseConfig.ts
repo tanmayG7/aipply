2b33e44f44cbef90c1988b140df4f73c13e8745b
Tanmay Garg <tanmaygarg01@gmail.com>
2025-07-07 21:04:36 +0530

    Update firebaseConfig.ts
    importing new types

--- lib/firebaseConfig/firebaseConfig.ts
+++ lib/firebaseConfig/firebaseConfig.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { UserSubscription, PLAN_FEATURES } from "../types";
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {
   getAuth,
