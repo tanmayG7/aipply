52657d7e9545c047e0d5a1cd6e551784638f622d
Tanmay Garg <tanmaygarg01@gmail.com>
2025-07-12 23:31:19 +0530

    Update PlatformCredentials.tsx

--- components/completeProfile/PlatformCredentials.tsx
+++ components/completeProfile/PlatformCredentials.tsx
@@ -141,9 +141,9 @@ const PlatformCredentials: React.FC<PlatformCredentialsProps> = ({
           <CardTitle className="text-[16px] font-inter font-semibold text-white">
             Platform Credentials
           </CardTitle>
-          <CardDescription className="font-inter text-[14px] leading-[20px] text-gray-400">
+          <p className="font-inter text-[14px] leading-[20px] text-gray-400">
             Store your job portal login credentials for easy access.
-          </CardDescription>
+          </p>
         </CardHeader>
         <CardContent className="col-span-5">
           {/* Security Note */}
