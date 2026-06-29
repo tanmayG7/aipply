f5cdfbf3f22c7339e4c180cdc55efbd5bc63de0b
Tanmay Garg <tanmaygarg01@gmail.com>
2026-06-29 11:10:13 +0530

    Change icon prop type to React.ElementType
    Signed-off-by: Tanmay Garg  <tanmaygarg01@gmail.com>

--- components/ui/feature-highlights.tsx
+++ components/ui/feature-highlights.tsx
@@ -28,7 +28,7 @@ export function FeatureHighlights() {
     );
 }
 
-function FeatureCard({ icon: Icon, title, desc }: { icon: any, title: string, desc: string }) {
+function FeatureCard({ icon: Icon, title, desc }: { icon: React.ElementType, title: string, desc: string }) {
     return (
         <div className="p-3 sm:p-4 rounded-xl bg-white/5 border border-white/10 hover:border-white/20 transition-all hover:bg-white/10 group flex flex-col items-start h-full">
             <Icon className="w-5 h-5 sm:w-6 sm:h-6 text-blue-400 mb-2 group-hover:scale-110 transition-transform" />
